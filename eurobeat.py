7b6896b453a142c4e6ddf8b1622a9ee8071adcb8
CRenwick57 <crenwick57@gmail.com>
2019-03-04 11:06:01 +0000

    Update eurobeat.py

--- eurobeat.py
+++ eurobeat.py
@@ -1,28 +1,44 @@
 from random import randint
 
-nouns = ['Night','Fire','Boy','Beat','Sun','Heart','Speed','Heartbeat']
-locations = ['Space','Tokyo','The 90s']
-verbs = ['Sleep','Dance','Rise','Beat','Remember','Run','Burn','Stand','Love']
+nouns = ['Night','Fire','Boy','Beat','Sun','Heart','Speed','Heartbeat','Love',
+            'Gas','Power','Race','Game','Car','Fantasy','Eurobeat']
+locations = ['Space','Tokyo','The 90s','Danger','Japan']
+verbs = ['Sleep','Dance','Rise','Beat','Remember','Run','Burn','Stand','Love',
+            'Kill']
 
 titles = [
     'noun1 of noun2',
     'location noun',
     'No One verb in location',
     'verbing',
-    'noun of the verbing noun',
+    'noun1 of the verbing noun2',
     'noun',
     'verb Me',
     'verbing in location',
     'verbing Up For You',
     'Don\'t verb So Close',
     'nounAY nounA noun',
-    'noun verber'
+    'noun verber',
+    'noun noun noun',
+    'verbing my noun',
+    'Get me noun',
+    'The noun1 is the noun2',
+    'The noun1 of the noun2',
+    'My noun1 is noun2',
+    'noun is in location',
+    'Need noun',
+    'Crazy for noun',
+    'verbing noun',
+    'location Fever',
+    'noun Flight to location',
+    'I need your noun',
+    'noun Rhapsody',
+    'Made in location',
+    'Disco noun',
+    'Super noun',
     ]
 
 def getWord(category,rule=None):
-    nouns = ['Night','Fire','Boy','Beat','Sun','Heart','Speed']
-    locations = ['Space','Tokyo','The 90s']
-    verbs = ['Sleep','Dance','Rise','Beat','Remember','Run','Burn','Stand','Love']
     w = ''
     if category == 'nouns':
         w = nouns[randint(0,len(nouns)-1)]
@@ -33,25 +49,31 @@ def getWord(category,rule=None):
         if rule == 'ing':
             if w[-1] is 'e':
                 w = w[:-1]
-            elif w in ['run']:
-                w+='n'
+            elif w in ['Run']:
+                w+=w[-1]
             w+='ing'
         elif rule is 'er':
-            if w in ['run']:
-                w+='n'
+            if w in ['Run']:
+                w+=w[-1]
             if w[-1] != 'e':
                 w+='e'
             w+='r'
     return w
 
 def addY(w):
-    if w is 'fire':
-        w = 'fier'
-    elif w in 'sun':
-        w+='n'
+    if w == 'Fire':
+        w = 'Fier'
+    elif w in ['Sun','Gas','Car']:
+        w+=w[-1]
+    elif w in ['Race']:
+        w = w[:-1]
     w+='y'
-    if w is 'boyy':
-        w = 'boyish'
+    if w == 'Boyy':
+        w = 'Boyish'
+    elif w == 'Powery':
+        w = 'Powerful'
+    elif w == 'Fantasyy':
+        w = 'Fantastical'
     return w
 
 
